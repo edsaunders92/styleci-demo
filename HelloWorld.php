640be184c7218ec906cb2f0a49131bcf5249a201
edsaunders92 <edsaunders92@users.noreply.github.com>
2021-09-29 07:06:31 +0000

    Apply fixes from StyleCI

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,23 +1,20 @@
 <?php
 
-class
-  
-HelloWorld 
-  extends
+class HelloWorld extends
   World
-  
 {
-  public function helloWorld() { return "hello world";}
-  
-      public function toString
-      ()
-        
-      {
-        return $this->helloWorld(); 
-      }
-  
-  public
-    function
-    isHelloWorld($string   ) 
-  { return $string == $this->helloWorld();}
+    public function helloWorld()
+    {
+        return 'hello world';
+    }
+
+    public function toString()
+    {
+        return $this->helloWorld();
+    }
+
+    public function isHelloWorld($string)
+    {
+        return $string == $this->helloWorld();
+    }
 }
