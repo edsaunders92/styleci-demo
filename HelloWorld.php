67df2db6067805d9e51ae38b8ea19ac06e98a53a
edsaunders92 <45391666+edsaunders92@users.noreply.github.com>
2021-09-29 08:06:15 +0100

    Styleci test

---
+++ HelloWorld.php
@@ -0,0 +1,23 @@
+<?php
+
+class
+  
+HelloWorld 
+  extends
+  World
+  
+{
+  public function helloWorld() { return "hello world";}
+  
+      public function toString
+      ()
+        
+      {
+        return $this->helloWorld(); 
+      }
+  
+  public
+    function
+    isHelloWorld($string   ) 
+  { return $string == $this->helloWorld();}
+}
